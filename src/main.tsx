60a714a34f869433ff01ece385dc1a4647848b29
simple-mq <simple-mq@qq.com>
2023-08-12 06:56:40 +0800

    img

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './tree.css';
 import App from './App';
-import init, { NBT } from 'nbtrock';
-await init();
+import { NBT } from 'nbtrock';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <App
     nbt={
